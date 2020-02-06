1626fa4557f6d6412a2d2c6f0548708a0efbd391
LukasLH27 <luheber@tsn.at>
2020-02-06 14:56:20 +0100

    Liste erzeugt

--- Notizen_Projekt/Notizen_Projekt/Controllers/HomeController.cs
+++ Notizen_Projekt/Notizen_Projekt/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Notizen_Projekt.Models;
 
 namespace Notizen_Projekt.Controllers
 {
@@ -10,8 +11,15 @@ namespace Notizen_Projekt.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<Note> notes = new List<Note> {
+                new Note("Lukas","Hallo",DateTime.Now,DateTime.Now,Status.publicN),
+                new Note("Lukas","Spalte 2",new DateTime(2019,12,31),DateTime.Now,Status.friendsN),
+                new Note("Lukas","abcdef",new DateTime(2012,10,14),new DateTime(2020,1,3),Status.privateN),
+                new Note("Lukas","efdsafs",new DateTime(2012,10,14),new DateTime(2020,1,3),Status.publicN)
+            };
+            return View(notes);
         }
 
+      
     }
 }
