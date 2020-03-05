b86ab48abab1563140671c2cc1a441177c20c5c2
elismasher <rist24082001@gmail.com>
2020-03-05 16:25:08 +0100

    Tabellen Tags und Notes hinzugefügt + Logout

--- Notizen_Projekt/Notizen_Projekt/Models/Note.cs
+++ Notizen_Projekt/Notizen_Projekt/Models/Note.cs
@@ -17,20 +17,6 @@ namespace Notizen_Projekt.Models
 
     public class Note
     {
-        private string _user;
-
-
-        public string User 
-        { 
-            get { return this._user; }
-            set 
-            { if (value != this._user ) 
-                {
-                    this._user = value;
-                } 
-            }
-        }
-
         public string NoteTitle { get; set; }
         public string NoteText { get; set; }
 
@@ -45,11 +31,10 @@ namespace Notizen_Projekt.Models
         public string Tag { get; set; }
 
 
-        public Note() : this("", "", "",DateTime.Now,DateTime.Now,Status.notSpecified, ColourNote.notSpecified, 0,"") { }
+        public Note() : this("", "",DateTime.Now,DateTime.Now,Status.notSpecified, ColourNote.notSpecified, 0,"") { }
 
-        public Note(string user,string noteTitle, string noteText, DateTime dateWritten, DateTime dateLastEdit, Status status, ColourNote colourNote, int id, string tag)
+        public Note(string noteTitle, string noteText, DateTime dateWritten, DateTime dateLastEdit, Status status, ColourNote colourNote, int id, string tag)
         {
-            this.User = user;
             this.NoteTitle = noteTitle;
             this.NoteText = noteText;
             this.DateWritten = dateWritten;
--- Notizen_Projekt/Notizen_Projekt/Models/db/RepositoryUser.cs
+++ Notizen_Projekt/Notizen_Projekt/Models/db/RepositoryUser.cs
@@ -11,7 +11,7 @@ namespace Notizen_Projekt.Models.db
     public class RepositoryUser : IRepositoryUser
     {
         //hier eigenen Login verwenden (für Datenbank)
-        private string _connectionString = "Server=localhost; Database=db_daten; Uid=root; Pwd=Klexi2408;";
+        private string _connectionString = "Server=localhost; Database=db_NotizenProjekt; Uid=root; Pwd=Klexi2408;";
         private MySqlConnection _connection;
 
         public void Open()
